339742cd7e30f8fb5f7cd6ca09d55b95bcbacde0
MrDisney <4nata422@gmail.com>
2021-12-24 23:21:55 +0200

    lab15

--- app/__init__.py
+++ app/__init__.py
@@ -35,4 +35,7 @@ def create_app(config_name='default'):
         from .posts import post_blueprint
         app.register_blueprint(post_blueprint, url_prefix='/post')
 
+        from .api import api_blueprint
+        app.register_blueprint(api_blueprint, url_prefix='/api')
+
         return app
